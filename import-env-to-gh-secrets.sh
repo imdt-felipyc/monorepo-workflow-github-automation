b15048e03725e76e9c18b8cb0aa2f5d0eaf6c885
felipy <felipyc@imdt.com.br>
2025-06-10 17:16:05 -0300

    add

--- import-env-to-gh-secrets.sh
+++ import-env-to-gh-secrets.sh
@@ -1,11 +1,22 @@
 #!/bin/bash
 
-# Substitua pelo caminho do seu repositório (ex: imdt-felipyc/monorepo-workflow-github-automation)
+# Replace with the path to your repository (e.g., imdt-felipyc/monorepo-workflow-github-automation)
 REPO="imdt-felipyc/monorepo-workflow-github-automation"
 
+# Deleting all existing variables
+echo "🗑️ Deleting variables from the .env file..."
+while IFS='=' read -r key _; do
+  if [[ ! -z "$key" && ! "$key" =~ ^# ]]; then
+    echo "❌ Removing $key"
+    gh variable delete "$key" --repo "$REPO"
+  fi
+done < .env
+
+# Adding new variables
+echo "🔐 Adding new variables..."
 while IFS='=' read -r key value; do
   if [[ ! -z "$key" && ! "$key" =~ ^# ]]; then
-    echo "🔐 Adicionando $key"
-    gh secret set "$key" --repo "$REPO" --body "$value"
+    echo "🔐 Adding $key"
+    gh variable set "$key" --repo "$REPO" --body "$value"
   fi
 done < .env
