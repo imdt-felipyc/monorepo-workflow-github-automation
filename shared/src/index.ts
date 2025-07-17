e371050c72ae5773be2d6a890e31302a1cb4329d
felipy <felipyc@imdt.com.br>
2025-07-17 18:28:47 -0300

    x

--- shared/src/index.ts
+++
@@ -1,6 +0,0 @@
-export * from './services/github.service.js'
-export * from './services/logger.js'
-export * from './services/notifier.js'
-export * from './services/sync.service.js'
-export * from './env.js'
-export * from './mockreq.js'
