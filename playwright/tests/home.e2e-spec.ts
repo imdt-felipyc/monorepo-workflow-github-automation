209ca7b00115128a45a9338f9497fda43705f80f
felipy <felipyc@imdt.com.br>
2025-07-18 15:43:21 -0300

    r

--- playwright/tests/home.e2e-spec.ts
+++ playwright/tests/home.e2e-spec.ts
@@ -2,21 +2,20 @@ import { test, expect } from '@playwright/test';
 
 test.describe('home', () => {
   test('should display the phrase "Formations Santé pour Tous" on the home page', async ({ page }) => {
-    await page.goto('/', { waitUntil: 'networkidle' });
+    await page.goto('/');
     await expect(page.getByText('Formations Santé pour Tous')).toBeVisible();
   });
-  
 
   test(
     'each Formations menu item page should contain the class course-atf__content',
     async ({ page }) => {
-      test.setTimeout(180000);
-      await page.goto('/', { waitUntil: 'networkidle' });
+      await page.goto('/');
+      test.setTimeout(600000);
 
-      // Aguarda o menu aparecer
+      // Wait for the menu to appear
       await page.waitForSelector('.dynamic-item.has-children .title .label', { timeout: 10000 });
 
-      // Seleciona o item "Formations" pelo texto
+      // Select the "Formations" menu item by text
       const formationsMenu = page.locator('.dynamic-item.has-children .title .label', { hasText: 'Formations' });
       await formationsMenu.hover();
 
@@ -28,13 +27,14 @@ test.describe('home', () => {
 
       for (const link of links) {
         const href = await link.getAttribute('href');
+        
         if (href) {
           const newPage = await page.context().newPage();
-          await newPage.goto(href, { waitUntil: 'networkidle', timeout: 60000});
+          await newPage.goto(href, { timeout: 60000 });
           await expect(newPage.locator('.course-atf__content')).toBeVisible({ timeout: 60000 });
           await newPage.close();
         }
       }
-    }
+    }, 
   );
 });
